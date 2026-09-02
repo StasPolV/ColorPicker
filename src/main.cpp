45ca605b49364a88df09660c9469a9f448fc764e
StasPolV <poliakov.stas.cpp@gmail.com>
2026-09-02 17:50:37 +0300

    feat: connect gui and core modules

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
+#include "ColorGrabber.h"
+#include "ColorPickerWidget.h"
+
 #include <QApplication>
+#include <QObject>
 
 int main(int argc, char* argv[])
 {
 	QApplication app(argc, argv);
+	ColorPickerWidget widget;
+	ColorGrabber color_grabber;
+	widget.show();
+
+	QObject::connect(&color_grabber, &ColorGrabber::ColorGrabbed, &widget,
+	                 &ColorPickerWidget::SetColor);
+
+	QObject::connect(&widget, &ColorPickerWidget::CustomMouseClicked, &color_grabber,
+	                 &ColorGrabber::GrabColor);
 
 	return app.exec();
 }
